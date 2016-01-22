f0bb3ef595aa61428ee805a25238694db507c607
Lisa Lin <lin.lisa@hotmail.com>
2016-01-21 19:41:32 -0500

    Add direct messaging and follower spamming

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -27,12 +27,39 @@ class MicroBlogger
 			case command
 				when 'q' then puts "Goodbye!"
 				when 't' then tweet(parts[1..-1].join(" "))
+				when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+				when 'spam' then spam_my_followers(parts[1..-1].join(" "))
 				else
 					puts "Sorry, I don't know how to #{command}"
 			end
 		end
 	end	
 	
+	def dm(target, message)
+		puts "Tryng to send #{target} this direct message:"
+		puts message
+		screen_names = @client.followers.collect { |follower| @client.user(follower).screen_name }
+		if screen_names.include? (target)
+			message = "d @#{target} #{message}"
+			tweet(message)
+		else
+			puts "Message not sent (target is not a follower)" 
+		end
+	end
+	
+	def followers_list
+		screen_names = []
+		@client.followers.each do |follower|
+			screen_names << @client.user(follower).screen_name
+		end
+		screen_names
+	end
+	
+	def spam_my_followers(message)
+		followers_list.each do |follower|
+			dm(follower, message)
+		end
+	end
 end
 
 blogger = MicroBlogger.new
