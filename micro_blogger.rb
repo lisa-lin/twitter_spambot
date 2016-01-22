3a76abece17a959d0c52acc298d58887d4aa3bbd
Lisa Lin <lin.lisa@hotmail.com>
2016-01-21 21:52:43 -0500

    Add automatic shortening of URLs with bitly

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -1,4 +1,5 @@
 require 'jumpstart_auth'
+require 'bitly'
 
 class MicroBlogger
 	attr_reader :client
@@ -30,6 +31,8 @@ class MicroBlogger
 				when 'dm' then dm(parts[1], parts[2..-1].join(" "))
 				when 'spam' then spam_my_followers(parts[1..-1].join(" "))
 				when 'elt' then everyones_last_tweet 
+				when 's' then shorten(parts[1..-1].join(" "))
+				when 'turl' then tweet(parts[1..-2].join(" ") + " " + shorten(parts[-1]))
 				else
 					puts "Sorry, I don't know how to #{command}"
 			end
@@ -70,6 +73,13 @@ class MicroBlogger
       		puts "#{@client.user(friend).screen_name} said this on #{timestamp.strftime("%A, %b, %d")}:"
       		puts "#{@client.user(friend).status.text}"
 		end
+	end
+	
+	def shorten(original_url)
+    	Bitly.use_api_version_3
+    	bitly = Bitly.new('hungryacademy', 'R_430e9f62250186d2612cca76eee2dbc6')
+    	puts "Shortening this URL: #{original_url}"
+    	bitly.shorten(original_url).short_url
 	end
 end
 
