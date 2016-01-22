1e2fd6f06cc9ae53f9c72128e9cf5c6df555194a
Lisa Lin <lin.lisa@hotmail.com>
2016-01-21 19:01:24 -0500

    Improve interface with run loop

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -9,9 +9,31 @@ class MicroBlogger
 	end
 	
 	def tweet(message)
-		@client.update(message)
+		if message.length <= 140
+			@client.update(message)
+		else
+			puts "This tweet is #{message.length - 14} characters too long. Please shorten the tweet in order to post."
+		end
 	end
+	
+	def run
+		puts "Welcome to the JSL Twitter Client!"
+		command = ""
+		while command != "q"
+			printf "enter command: "
+			input = gets.chomp
+			parts = input.split(" ")
+			command = parts[0]
+			case command
+				when 'q' then puts "Goodbye!"
+				when 't' then tweet(parts[1..-1].join(" "))
+				else
+					puts "Sorry, I don't know how to #{command}"
+			end
+		end
+	end	
+	
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("MicroBlogger Initialized")
+blogger.run
